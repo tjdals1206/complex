7178c49d3d4accf108d363ad746460b454c8a837
ch9416 <1ee6@naver.com>
2022-07-04 16:42:37 +0900

    add function added

--- complex.py
+++ complex.py
@@ -16,19 +16,15 @@ class Complex:
     def __str__(self):
         return str(self.re)+"+"+str(self.im)+"i"
         
-class Addition:
-    def __init__(self, re=0, im=0):
-        self.re = re
-        self.im = im
-    
-    
-    def __str__(self):
-        return str(self.re+self.im)
            
-                
+    def add(self, c1):
+        c.re = self.re + c1.re
+        c.im = self.im + c1.im
+        return c
         
 c1 = Complex(1, 2)
-c2 = Addition(1, 2)
 print(c1)
+
+c2 = Complex(2, 3)
 print(c2)
 
