d9434c6dcbefe10385d77ba8459fe501df611ae8
Kim Dong Han <83560202+ch9416@users.noreply.github.com>
2022-07-04 17:02:44 +0900

    Update complex.py

--- complex.py
+++ complex.py
@@ -1,7 +1,7 @@
 """
-name1: 문성민
-name2:addition,subtraction
-name3:multiplication
+문성민: maintainer
+김동한: addition,subtraction
+조민식: multiplication
 """
 class Complex:
     def __init__(self, re, im):
@@ -20,4 +20,4 @@ class Complex:
 c1 = Complex(1,2)
 print(c1)
 c2 = Complex(2, 3)
-print(c2.subtract(c2))
+print(c1.subtract(c2))
