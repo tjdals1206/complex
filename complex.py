685b8e8dd15ea0646852309ecb607f0dc78ae68b
Seongmin Moon <tjdals1206@hufs.ac.kr>
2022-07-04 16:28:11 +0900

    base complex class added

--- complex.py
+++ complex.py
@@ -1,7 +1,7 @@
 """
-name1: 문성민
-name2:addition,subtraction
-name3:multiplication
+문성민: maintainer
+김동한:addition,subtraction
+조민식:multiplication
 """
 class Complex:
     def __init__(self, re, im):
